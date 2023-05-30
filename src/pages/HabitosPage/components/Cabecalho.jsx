3befbd65bf0a5e32341da6852e1f97168bb64e11
levymc <levymcruz@gmail.com>
2023-05-30 14:08:53 -0300

    att estilos cabeçalho

--- src/pages/HabitosPage/components/Cabecalho.jsx
+++ src/pages/HabitosPage/components/Cabecalho.jsx
@@ -16,13 +16,20 @@ const ContainerCabecalho = styled.div`
     gap: 50vw;
     align-items: center;
     justify-content: center;
+    h1{
+        font-size: 22.976px;
+        line-height: 29px;
+        color: #126BA5;
+    }
 `
 
 const SCButton = styled.button`
     width: 2.5rem;
     height: 2.1875rem;
-    font-size: 16px;
+    font-size: 18px;
     background-color: #52B6FF;
     border: 1px solid #52B6FF;
     border-radius: 5px;
+    color: white;
+    font-weight: 700;
 `
